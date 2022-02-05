5545555f08d341e286a4359a2f388ab2572efe93
dharan1999 <dharanthakar96@gmail.com>
2022-02-05 10:58:26 -0500

    First commit

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:dharanthakar96.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/dharan1999',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'https://www.linkedin.com/in/dharan-thaker',
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: 'https://twitter.com/join_the_run',
+    image: twitterIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
